dda443bea074fbf4f7d1b27043a787505dc7755a
NushratNowrin <nowrin2023@gmail.com>
2024-09-19 00:28:41 +0600

    country select option ready

--- JS/select.js
+++ JS/select.js
@@ -1,1 +1,11 @@
-function populateCountries() {}
+function populateCountries() {
+	const countrySelect = document.getElementById("selectCountry");
+	axios.get("https://restcountries.com/v3.1/all").then((res) => {
+		res.data.forEach((country) => {
+			const option = document.createElement("option");
+			option.value = country.name.common;
+			option.textContent = country.name.common;
+			countrySelect.appendChild = option;
+		});
+	});
+}
