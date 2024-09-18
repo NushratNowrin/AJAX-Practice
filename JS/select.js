e4a6ef3fa3000ae87a68d3e89cbadb78af5020d7
NushratNowrin <nowrin2023@gmail.com>
2024-09-19 00:32:01 +0600

    catch error for country search

--- JS/select.js
+++ JS/select.js
@@ -1,12 +1,14 @@
 function populateCountries() {
 	const countrySelect = document.getElementById("selectCountry");
 	axios.get("https://restcountries.com/v3.1/all").then((res) => {
-		res.data.forEach((country) => {
-			const option = document.createElement("option");
-			option.value = country.name.common;
-			option.textContent = country.name.common;
-			countrySelect.appendChild = option;
-		});
+		res.data
+			.forEach((country) => {
+				const option = document.createElement("option");
+				option.value = country.name.common;
+				option.textContent = country.name.common;
+				countrySelect.appendChild = option;
+			})
+			.catch((err) => console.log(err));
 	});
 }
 window.onload = populateCountries();
